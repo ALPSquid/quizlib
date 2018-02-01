cabe1fd3962d9a2ff3766a8ab8743b02eca026c5
ALPSquid <alpsquid@kolabnow.com>
2018-02-01 21:26:08 +0000

    Fixes main script

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,9 @@
 var quiz;
 
 function showResults() {
+    // Check answers and continue if all questions have been answered
     if (quiz.checkAnswers()) {
-        var quizScorePercent = quiz.result.scorePercentFormatted;
+        var quizScorePercent = quiz.result.scorePercentFormatted; // The unformatted percentage is a decimal in range 0 - 1
         var quizResultElement = document.getElementById('quiz-result');
         quizResultElement.style.display = 'block';
         document.getElementById('quiz-score').innerHTML = quiz.result.score.toString();
@@ -14,30 +15,32 @@ function showResults() {
         else if (quizScorePercent >= 50) quizResultElement.style.backgroundColor = '#ffc107';
         else if (quizScorePercent >= 25) quizResultElement.style.backgroundColor = '#ff9800';
         else if (quizScorePercent >= 0) quizResultElement.style.backgroundColor = '#f44336';
-
+        
+        // Highlight questions according to whether they were correctly answered. The callback allows us to highlight/show the correct answer
         quiz.highlightResults(handleAnswers);
     }
 }
 
-/** Callback for Quiz.highlightResults. Highlights the correct answers of incorrectly answered questions */
-function handleAnswers(question, no, correct) {
+/** Callback for Quiz.highlightResults. Highlights the correct answers of incorrectly answered questions 
+ * Parameters are: the quiz object, the question element, question number, correctly answered flag
+ */
+function handleAnswers(quiz, question, no, correct) {
     if (!correct) {
         var answers = question.getElementsByTagName('input');
         for (var i = 0; i < answers.length; i++) {
-            switch (answers[i].type) {
-                case "checkbox":
-                case "radio":
-                    if (quiz.answers[no].indexOf(answers[i].value) > -1) {
-                        answers[i].parentNode.classList.add(quiz.Classes.CORRECT);
-                    }
-                    break;
-                default:
-                    var correctAnswer = document.createElement('span');
-                    correctAnswer.classList.add(quiz.Classes.CORRECT);
-                    correctAnswer.classList.add(quiz.Classes.TEMP);
-                    correctAnswer.innerHTML = quiz.answers[no];
-                    correctAnswer.style.marginLeft = '10px';
-                    answers[i].parentNode.insertBefore(correctAnswer, answers[i].nextSibling);
+            if (answers[i].type === "checkbox" || answers[i].type === "radio"){ 
+                // If the current input element is part of the correct answer, highlight it
+                if (quiz.answers[no].indexOf(answers[i].value) > -1) {
+                    answers[i].parentNode.classList.add(Quiz.Classes.CORRECT);
+                }
+            } else {
+                // If the input is anything other than a checkbox or radio button, show the correct answer next to the element
+                var correctAnswer = document.createElement('span');
+                correctAnswer.classList.add(Quiz.Classes.CORRECT);
+                correctAnswer.classList.add(Quiz.Classes.TEMP); // quiz.checkAnswers will automatically remove elements with the temp class
+                correctAnswer.innerHTML = quiz.answers[no];
+                correctAnswer.style.marginLeft = '10px';
+                answers[i].parentNode.insertBefore(correctAnswer, answers[i].nextSibling);
             }
         }
     }
